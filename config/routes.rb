60e285f07647a110155fb0aeb292fc032892d99f
Kevin Agoh <kevinagoh@hotmail.fr>
2021-11-24 02:17:19 +0100

    displayed the restaurants on the show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'cities#index'
-  resources :cities, only: [:index]
+  resources :cities, only: %i[index show] do
+    resources :restaurants, only: [:index]
+  end
 end
