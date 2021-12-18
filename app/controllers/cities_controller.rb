64c173849f6532433aa43ee85296efb6072ae243
Kevin Agoh <kevinagoh@hotmail.fr>
2021-12-18 13:40:38 +0100

    done new seeds

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -4,8 +4,8 @@ class CitiesController < ApplicationController
   end
 
   def show
-    # I think the problem comes from here
     @city = City.find(params[:id])
+    @restaurant = Restaurant.find(params[:id])
     @restaurants = Restaurant.all
     @hotels = Hotel.all
     @clubs = Club.all
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,104 @@
 
 puts 'Creating cities...'
 
-City.create(name: 'Paris')
-City.create(name: 'London')
-City.create(name: 'Barcelona')
-City.create(name: 'New York')
-City.create(name: 'Los Angeles')
+City.create!(name: 'Paris')
+City.create!(name: 'London')
+City.create!(name: 'Barcelona')
+City.create!(name: 'New York')
+City.create!(name: 'Los Angeles')
 
-puts 'Creating restaurants...'
+puts 'Creating Paris restaurants...'
 
-Restaurant.create(name: 'Epoca', address: '17 Rue Oudinot, 75007 Paris, France', city_id: 1)
-Restaurant.create(name: 'Ida Restorante', address: '117 Rue de Vaugirard, 75015 Paris, France', city_id: 1)
-Restaurant.create(name: 'Papadoom Kitchen', address: '157 Rue Montmartre, 75002 Paris, France', city_id: 1)
-Restaurant.create(name: 'Rebellato', address: '136 Rue de la Pompe, 75116 Paris, France', city_id: 1)
+Restaurant.create!(name: 'Epoca', address: '17 Rue Oudinot, 75007 Paris, France', city_id: 1)
+Restaurant.create!(name: 'Ida Restorante', address: '117 Rue de Vaugirard, 75015 Paris, France', city_id: 1)
+Restaurant.create!(name: 'Papadoom Kitchen', address: '157 Rue Montmartre, 75002 Paris, France', city_id: 1)
+Restaurant.create!(name: 'Rebellato', address: '136 Rue de la Pompe, 75116 Paris, France', city_id: 1)
+
+puts 'Creating Paris hotels'
+
+Hotel.create!(name: 'Hyatt Regency Paris Étoile', address: '3 Pl. du Général Kœnig, 75017 Paris, France', city_id: 1)
+Hotel.create!(name: "OKKO Hotels Paris Gare de l'Est", address: "23 Rue d'Alsace, 75010 Paris, France", city_id: 1)
+
+puts 'Creating Paris bars'
+
+Club.create!(name: 'Le Royal Bar', address: '124 Blvd Marguerite de Rochechouart, 75018 Paris, France', city_id: 1)
+Club.create!(name: "Harry's New York Bar", address: '5 Rue Daunou, 75002 Paris, France', city_id: 1)
+Club.create!(name: 'Bar 19.20',
+             address: 'Prince de Galles, a Luxury Collection Hotel, Paris, 33 Av. George V, 75008 Paris, France', city_id: 1)
+
+puts 'Creating London restaurants...'
+
+10.times do
+  Restaurant.create!(name: Faker::Restaurant.name, address: "#{Faker::Address.street_address}, London, UK", city_id: 2)
+end
+
+puts 'Creating London hotels'
+
+10.times do
+  Hotel.create!(name: Faker::Restaurant.name, address: "#{Faker::Address.street_address}, London, UK", city_id: 2)
+end
+
+puts 'Creating London bars'
+
+10.times do
+  Club.create!(name: Faker::Restaurant.name, address: "#{Faker::Address.street_address}, London, UK", city_id: 2)
+end
+
+puts 'Creating Barcelona restaurants...'
+
+10.times do
+  Restaurant.create!(name: Faker::Restaurant.name, address: "#{Faker::Address.street_address}, Barcelona, Spain",
+                     city_id: 3)
+end
+
+puts 'Creating Barcelona hotels'
+
+10.times do
+  Hotel.create!(name: Faker::Restaurant.name, address: "#{Faker::Address.street_address}, Barcelona, Spain", city_id: 3)
+end
+
+puts 'Creating Barcelona bars'
+
+10.times do
+  Club.create!(name: Faker::Restaurant.name, address: "#{Faker::Address.street_address}, Barcelona, Spain", city_id: 3)
+end
+
+puts 'Creating New York restaurants...'
+
+10.times do
+  Restaurant.create!(name: Faker::Restaurant.name, address: "#{Faker::Address.street_address}, New York, USA",
+                     city_id: 4)
+end
+
+puts 'Creating New York hotels'
+
+10.times do
+  Hotel.create!(name: Faker::Restaurant.name, address: "#{Faker::Address.street_address}, New York, USA", city_id: 4)
+end
+
+puts 'Creating New York bars'
+
+10.times do
+  Club.create!(name: Faker::Restaurant.name, address: "#{Faker::Address.street_address}, New York, USA", city_id: 4)
+end
+
+puts 'Creating Los Angeles restaurants...'
+
+10.times do
+  Restaurant.create!(name: Faker::Restaurant.name, address: "#{Faker::Address.street_address}, Los Angeles, USA",
+                     city_id: 5)
+end
+
+puts 'Creating Los Angeles hotels'
+
+10.times do
+  Hotel.create!(name: Faker::Restaurant.name, address: "#{Faker::Address.street_address}, Los Angeles, USA", city_id: 5)
+end
+
+puts 'Creating Los Angeles bars'
+
+10.times do
+  Club.create!(name: Faker::Restaurant.name, address: "#{Faker::Address.street_address}, Los Angeles, USA", city_id: 5)
+end
 
 puts '...done !'
